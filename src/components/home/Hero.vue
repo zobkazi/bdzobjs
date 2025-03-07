bede45fff71693265e633fde81b7609979fce9ed
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-03-07 20:43:24 +0600

    hero images add

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -13,7 +13,7 @@
   
       <!-- Right Image -->
       <div class="mt-10 md:mt-0 w-full md:w-1/2 flex justify-center">
-        <img src="https://cwiekqazbhwtjgasjdjd.supabase.co/storage/v1/object/sign/user2/Screenshot%20From%202025-02-24%2013-21-35.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJ1c2VyMi9TY3JlZW5zaG90IEZyb20gMjAyNS0wMi0yNCAxMy0yMS0zNS5wbmciLCJpYXQiOjE3NDA4MzY1NDQsImV4cCI6MTc3MjM3MjU0NH0.-sfNYcYceePyTetWxrLIgXkZzubJ3-0QkSMY0xEbFTM" alt="Hero Image" class="max-w-sm md:max-w-lg rounded-lg shadow-lg" />
+        <img src="https://wjqnworwlgsmsedhhneu.supabase.co/storage/v1/object/sign/heroImages/hero/zobkazi_hero.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJoZXJvSW1hZ2VzL2hlcm8vem9ia2F6aV9oZXJvLnBuZyIsImlhdCI6MTc0MTM1ODUyNiwiZXhwIjoxNzcyODk0NTI2fQ.ds_YYDrJ_ARS4ZGRSiE43rmjdJ7b7kbzoqIvyoGVoK8" alt="Hero Image" class="max-w-sm md:max-w-lg rounded-lg shadow-lg" />
       </div>
     </section>
 
